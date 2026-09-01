b6242d7a4de907d714b963e18853b4a5cee2aea3
Dewa M. Widyakumara <widyakumara@users.noreply.github.com>
2026-09-01 17:03:43 +0700

    chore: init (#1)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,37 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import cloudflare from "@astrojs/cloudflare";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    port: 9999,
+    allowedHosts: ["demo.widyakumara.com"],
+  },
+  devToolbar: {
+    enabled: false,
+  },
+  outDir: "./.dist",
+  adapter: cloudflare(),
+  build: {
+    inlineStylesheets: "never",
+    format: "file",
+    assets: "inc",
+  },
+  vite: {
+    plugins: [tailwindcss()],
+    esbuild: { legalComments: "none" },
+    build: {
+      emptyOutDir: true,
+      rollupOptions: {
+        output: {
+          hashCharacters: "base36",
+          assetFileNames: "inc/[hash:8].[ext]",
+          chunkFileNames: "inc/c[hash:7].js",
+          entryFileNames: "inc/e[hash:7].js",
+        },
+      },
+    },
+  },
+});
---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+import Page from "./page.astro";
+
+export { Page };
